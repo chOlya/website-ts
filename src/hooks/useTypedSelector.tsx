86462278afcdfe3381c4ed5ac0e9c154044b0256
chOlya <Chvyrova.o@mail.ru>
2022-03-21 17:24:46 +0300

    Delete Redux

--- src/hooks/useTypedSelector.tsx
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { ReduceState } from "../redux/redux-store";
-
-
-export const useTypedSelector: TypedUseSelectorHook<ReduceState> = useSelector;
